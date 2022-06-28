02f8b7f2d3f12c6c81987b3dcfb545a468bff8e5
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-06-27 21:04:53 -0500

    generate rntuple with array (incompleted)

---
+++ data/GenRNTupleArray.C
@@ -0,0 +1,20 @@
+#include <ROOT/RField.hxx>
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <ROOT/RNTupleOptions.hxx>
+
+using RCollectionNTupleWriter = ROOT::Experimental::RCollectionNTupleWriter;
+using REntry = ROOT::Experimental::REntry;
+using RFieldBase = ROOT::Experimental::Detail::RFieldBase;
+using RNTupleModel = ROOT::Experimental::RNTupleModel;
+using RNTupleWriteOptions = ROOT::Experimental::RNTupleWriteOptions;
+using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
+
+void GenRNTupleArray()
+{
+    auto model = RNTupleModel::Create();
+
+    auto fldAx = model->MakeField<float*>("Array_x");
+
+    
+}
