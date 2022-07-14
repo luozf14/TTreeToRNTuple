58d1b2d9d329cd299445d73f93250e408dbc2e1b
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-07-14 18:25:14 -0500

    add std::string

--- data/GenTTreeContainerVector.C
+++ data/GenTTreeContainerVector.C
@@ -7,11 +7,14 @@ void GenTTreeContainerVector()
     std::vector<bool> vec_bool;
     ROOT::RVec<double> RVec_double;
     ROOT::RVec<std::string> RVec_string;
+    std::string string_1;
 
     tree->Branch("vec_float","std::vector<Float_t>",&vec_float);
     tree->Branch("vec_bool",&vec_bool);
     tree->Branch("RVec_double",&RVec_double);
     tree->Branch("RVec_string",&RVec_string);
+    tree->Branch("string_1","std::string",&string_1);
+
 
 
 
@@ -29,6 +32,7 @@ void GenTTreeContainerVector()
         for(int k=0;k<20;k++) vec_bool.push_back((ranGen.Rndm()*10)>5 ? true:false);
         for(int k=0;k<nX;k++) RVec_double.push_back(ranGen.Rndm()*100);
         for(int k=0;k<5;k++) RVec_string.push_back(std::to_string(ranGen.Rndm()*100));
+        string_1 = std::to_string(i);
         
         tree->Fill();
     }
