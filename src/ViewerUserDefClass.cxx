36e63da2c3a9ae5a9a07969e4432f9a290667273
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-07-26 17:21:11 -0500

    view RNtuple containing user defined class

---
+++ src/ViewerUserDefClass.cxx
@@ -0,0 +1,34 @@
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <TFile.h>
+#include "TSystem.h"
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+using RNTupleReader = ROOT::Experimental::RNTupleReader;
+using ENTupleShowFormat = ROOT::Experimental::ENTupleShowFormat;
+
+int main(int argc, char **argv)
+{
+    
+    if (argc != 2)
+    {
+        std::cout << "Error! Please specify the location of the output file!" << std::endl;
+        std::cout << "Example: ./Viewer output_file" << std::endl;
+        return 0;
+    }
+    gSystem->Load("../data/SimpleClass_cxx.so");
+
+    char const *kNTupleFileName = argv[1];
+
+    std::unique_ptr<TFile> ntupleFile(TFile::Open(kNTupleFileName));
+    std::string ntupleName = ntupleFile->GetListOfKeys()->First()->GetName();
+
+    auto ntuple = RNTupleReader::Open(ntupleName, kNTupleFileName);
+    ntuple->PrintInfo();
+    std::cout << "The third entry is shown below:" << std::endl;
+    ntuple->Show(19, ENTupleShowFormat::kCompleteJSON);
+    return 0;
+}
