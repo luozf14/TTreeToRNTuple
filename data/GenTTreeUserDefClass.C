31b34c35b6ed0d5c875d99037cf58fb3d0fbb9c7
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-08-25 12:53:20 -0500

    add nested std::vector

--- data/GenTTreeUserDefClass.C
+++ data/GenTTreeUserDefClass.C
@@ -12,23 +12,32 @@ void GenTTreeUserDefClass()
 
     SimpleClass simpleClass;
 
-    tree->Branch("SimpleClass","SimpleClass",&simpleClass);
+    tree->Branch("simpleClass","SimpleClass",&simpleClass);
     
     TRandom3 ranGen;
-    Int_t nX;
+    Int_t nX,nY;
     std::vector<Double_t> tempVecDouble;
+    std::vector<std::vector<float>> tempVecVecfloat;
     for(int i=0; i<20; i++)
     {
         simpleClass.SetInt(i);
         simpleClass.SetFloat(ranGen.Rndm()*10);
         nX=(Int_t)(ranGen.Rndm()*10);
-        printf("%d\n",nX);
+        nY=(Int_t)(ranGen.Rndm()*10);
         tempVecDouble.clear();
+        tempVecVecfloat.clear();
         for(int j=0;j<nX;j++) 
         {
             tempVecDouble.push_back(ranGen.Gaus());
+            std::vector<float> temp;
+            for (int k = 0; k < nY; k++)
+            {
+                temp.push_back(ranGen.Rndm());
+            }
+            tempVecVecfloat.push_back(temp);
         }
         simpleClass.SetVecDouble(tempVecDouble);
+        simpleClass.SetVecVecFloat(tempVecVecfloat);
         tree->Fill();
     }
 
