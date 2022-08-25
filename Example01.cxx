80b643a440374ac613cde4eabd0c3b9afc943d66
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-08-25 12:46:03 -0500

    example of using this tool as a library

---
+++ Example01.cxx
@@ -0,0 +1,39 @@
+#include "TTreeToRNTuple.hxx"
+
+#include <string>
+#include <vector>
+
+using RNTupleReader = ROOT::Experimental::RNTupleReader;
+using ENTupleShowFormat = ROOT::Experimental::ENTupleShowFormat;
+
+int main(int argc, char **argv)
+{
+    std::string inputFile = "../data/TTreeMixed.root";
+    std::string outputFile = "out.ntuple";
+    std::string treeName = "MixedTree";
+    std::string compressionAlgo = "zstd";
+    std::vector<std::string> dictionary = {"../data/SimpleClass_cxx.so"};
+
+    //convert
+    std::unique_ptr<TTreeToRNTuple> conversion = std::make_unique<TTreeToRNTuple>(inputFile, outputFile, treeName);
+    // conversion->SetInputFile(inputFile);
+    // conversion->SetOutputFile(outputFile);
+    // conversion->SetTreeName(treeName);
+    conversion->SetCompressionAlgo(compressionAlgo);
+    conversion->SetDictionary(dictionary);
+    conversion->EnableMultiThread();
+    conversion->Convert();
+
+    //view
+    std::unique_ptr<TFile> ntupleFile(TFile::Open(outputFile.c_str()));
+    std::string ntupleName = ntupleFile->GetListOfKeys()->First()->GetName();
+    auto ntuple = RNTupleReader::Open(ntupleName, outputFile);
+    ntuple->PrintInfo();
+    int nEntry = 13;
+    printf("The %dth entry is shown below:\n", nEntry);
+    ntuple->Show(nEntry, ENTupleShowFormat::kCompleteJSON);
+    
+    return 1;
+}
+
+   
