1b8fb243d831931ca61ec933a6c35a09b79c9731
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-09-21 16:38:03 -0500

    add function of only convert user-specified subbranches

--- Example01.cxx
+++ Example01.cxx
@@ -9,17 +9,19 @@ using ENTupleShowFormat = ROOT::Experimental::ENTupleShowFormat;
 int main(int argc, char **argv)
 {
     std::string inputFile = "../data/TTreeMixed.root";
-    std::string outputFile = "out";
+    std::string outputFile = "output.ntuple";
     std::string treeName = "MixedTree";
-    std::string compressionAlgo = "zstd";
+    std::string compressionAlgo = "none";
     int compressionLevel = 0;
     std::vector<std::string> dictionary = {"../data/SimpleClass_cxx.so"};
+    std::vector<std::string> subBranches = {"simpleClass","x"};
     // bool enableMtiltiThread = false;
 
     //convert
     std::unique_ptr<TTreeToRNTuple> conversion = std::make_unique<TTreeToRNTuple>(inputFile, outputFile, treeName);
     conversion->SetCompressionAlgoLevel(compressionAlgo, compressionLevel);
     conversion->SetDictionary(dictionary);
+    conversion->SetSubBranch(subBranches);
     // conversion->EnableMultiThread(enableMtiltiThread);
     conversion->Convert();
 
@@ -28,7 +30,7 @@ int main(int argc, char **argv)
     std::string ntupleName = ntupleFile->GetListOfKeys()->First()->GetName();
     auto ntuple = RNTupleReader::Open(ntupleName, outputFile);
     ntuple->PrintInfo();
-    int nEntry = 106259;
+    int nEntry = 200;
     printf("The %dth entry is shown below:\n", nEntry);
     ntuple->Show(nEntry, ENTupleShowFormat::kCompleteJSON);
     
