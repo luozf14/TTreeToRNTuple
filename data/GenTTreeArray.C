2373e9200313d2799cec0641f7f0ba7f1229203a
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-10-09 17:26:48 -0500

    increase entries

--- data/GenTTreeArray.C
+++ data/GenTTreeArray.C
@@ -1,7 +1,7 @@
 void GenTTreeArray()
 {
     auto file = std::make_shared<TFile>("TTreeArray.root", "RECREATE");
-   auto tree = std::make_shared<TTree>("T2", "TTree with c++ arrays");
+    auto tree = std::make_shared<TTree>("T2", "TTree with c++ arrays");
 
     Float_t x[3];
     Double_t y[5];
@@ -13,22 +13,24 @@ void GenTTreeArray()
     tree->Branch("y", y, "y[5]/D");
     tree->Branch("nZ", &nZ, "nZ/I");
     tree->Branch("z", z, "z[nZ]/D");
-    tree->Branch("array_float",&array_float);
+    tree->Branch("array_float", &array_float);
 
     TRandom3 ranGen;
-    for(int i=0; i<10; i++)
+    for (int i = 0; i < 2000; i++)
     {
-        for(int j=0;j<5;j++)
+        for (int j = 0; j < 5; j++)
         {
-            if(j<3) x[j]=ranGen.Gaus(0,1);
-            y[j]=ranGen.Rndm(2.);
+            if (j < 3)
+                x[j] = ranGen.Gaus(0, 1);
+            y[j] = ranGen.Rndm(2.);
         }
-        nZ=(Int_t)(ranGen.Rndm()*10);
-        for(Int_t k=0;k<nZ;k++)
+        nZ = (Int_t)(ranGen.Rndm() * 10);
+        for (Int_t k = 0; k < nZ; k++)
         {
-            z[k]=ranGen.Rndm();
+            z[k] = ranGen.Rndm();
         }
-        for(int k=0;k<10;k++) array_float[k]=(ranGen.Rndm()*100);
+        for (int k = 0; k < 10; k++)
+            array_float[k] = (ranGen.Rndm() * 100);
 
         tree->Fill();
     }
